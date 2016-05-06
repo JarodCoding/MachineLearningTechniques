57e0ddd306fcedbbc52197ac2680716941526a6d
JarodCoding <jardcoding@gmail.com>
2016-05-06 18:33:01 +0200

    Added Copyright, Added Automatic k selection (squareroot of featuresize/trainingsdata amount)

--- KNN/K_Nearest_Neighbours.java
+++ KNN/K_Nearest_Neighbours.java
@@ -6,13 +6,18 @@ import lib.jadsl.collections.list.StaticSizeArrayList;
 import java.util.*;
 
 /**
- * Created by Pascal Jarod Kuthje on 28.04.2016.
+ * Created by Pascal Jarod Kuthe on 28/04/2016.
+ * Copyright (c) <2016> <Pascal Jarod Kuthe>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-public abstract class K_Nearest_Neighbors {
+public abstract class K_Nearest_Neighbours {
 
     private int dimension;
     private DataVectorType type;
     private boolean running = false;
+    private int totalSize = 0;
 
     public K_Nearest_Neighbors(int dimension,DataVectorType type){
         this.dimension = dimension;
@@ -26,11 +31,13 @@ public abstract class K_Nearest_Neighbors {
             if(classification < trainedData.size())throw new IllegalArgumentException("Classifications need to be registered before they are used! Highest registered classification is: \"+NextClassification+\" given was: \"+classification");
             if(running)throw new IllegalAccessError("Training phase is over!");
             trainedData.get(classification).addAll(Arrays.asList(xn));
+            totalSize += xn.length;
         }
         public void train(int classification, Collection<DataVector> xn){
             if(classification < trainedData.size())throw new IllegalArgumentException("Classifications need to be registered before they are used! Highest registered classification is: \"+NextClassification+\" given was: \"+classification");
             if(running)throw new IllegalAccessError("Training phase is over!");
             trainedData.get(classification).addAll(xn);
+            totalSize += xn.size();
         }
 
         public int getHighestClassification(){
@@ -47,10 +54,6 @@ public abstract class K_Nearest_Neighbors {
     public int classify(DataVector x,int k){
         running = true;
         //check if enough training data is present
-            int totalSize = 0;
-            for(Collection c:trainedData){
-                totalSize += c.size();
-            }
             if(totalSize<k){
                 running = false;
                 throw new IllegalStateException("There need to be at least k training data available!");
@@ -63,6 +66,9 @@ public abstract class K_Nearest_Neighbors {
         int res = selectClassification(closestClassifications);
         running = false;
         return res;
+    }
+    public int classify(DataVector x){
+        return classify(x,(int)Math.round(Math.sqrt(totalSize)));
     }
     protected abstract int selectClassification(Integer[] closestClassifications);
     private Integer[] select_K_closestClassifications(DataVector x,int k){
